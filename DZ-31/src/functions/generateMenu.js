afbaf38345cdb70405ba9f4feff7759c8cdc54e5
vladimir_sh <vladimir.shkarupin@cprime.com>
2023-09-19 12:53:45 +0300

    Unified code style

--- DZ-31/src/functions/generateMenu.js
+++ DZ-31/src/functions/generateMenu.js
@@ -1,18 +1,19 @@
 "use strict";
+
 function generateMenu(productsList) {
   const result = {};
   const categories = new Set();
   products.forEach((e) => categories.add(e.category));
-  const nav = tag("nav", "", { className: "appshop__menu" });
-  const ul = tag("ul", "", { className: "appshop__list" });
+  const nav = tag("nav", "", {className: "appshop__menu"});
+  const ul = tag("ul", "", {className: "appshop__list"});
   categories.forEach((element) => {
     const li = tag("li");
     li.append(
       tag(
         "a",
         element,
-        { href: `index.html?category=${element}`, className: "appshop__menulinks", title: `Categoty: ${element}` },
-        { "data-category": element }
+        {href: `index.html?category=${element}`, className: "appshop__menulinks", title: `Categoty: ${element}`},
+        {"data-category": element}
       )
     );
     ul.append(li);
